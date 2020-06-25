6f1949da4443424dd9cac8452ebc05c602e21bba
Evance Soumaoro <evanxg852000@yahoo.fr>
2020-06-25 12:42:04 +0100

    added log to http function

--- functions.js
+++ functions.js
@@ -1,5 +1,7 @@
 //http function
 function API(req, res) {
+    const ip = res.socket.remoteAddress;
+    console.log(`Client ip address is ${ip}.`);
     res.writeHead(200);
     res.end('Hello, World!');
 }
